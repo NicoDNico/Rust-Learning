32760acc8e2bf6bdc9070f738cb6ca889f0d2818
NicoDNico <Nicolasdnicora@gmail.com>
2023-03-31 02:36:10 -0300

    Guesses
    Not gonna lie i thought this was gonna be easier. i gotta import compare ? i get it but i hate it.

--- Guesses/src/main.rs
+++ Guesses/src/main.rs
@@ -1,13 +1,30 @@
-use std::io::{self, Read};
-
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
 
 fn main() {
-    println!("Adivina el numero, Gana 2 pesos");
-
+    println!("Adivina el numero, Gana 2 pesos:");
+    let secret = rand::thread_rng().gen_range(1..=100);
+    loop{
+    
     let mut guess = String::new();
 
-    io::stdin().read_line(&mut guess).expect("Fail");
 
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Fail");
+
+    let guess: i32 = match guess.trim().parse() {
+        Ok(num) => num,
+        Err(_) => continue,
+    };
+    
     println!("Dijiste {guess}");
 
+    match guess.cmp(&secret){
+            Ordering::Equal =>{println!("Le pegaste");break;},
+            Ordering::Greater=>println!("Muy alto"),
+            Ordering::Less=>println!("Muy bajo"),
+    }
+}
 }
