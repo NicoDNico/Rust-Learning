ece1ea769b922d382fc6c53ef31500c04b27ca77
NicoDNico <Nicolasdnicora@gmail.com>
2023-04-01 04:01:25 -0300

    Maybe type safety was a mistake
    I know the difference between &str and String for sure however using magic values for testing is not fun here in rust.

--- tests/src/main.rs
+++ tests/src/main.rs
@@ -1,11 +1,21 @@
 
+struct Info {
+    mail: String,
+    name: String,
+    age: u8,
+}
 
 fn main() {
-    let x:String = String::from("Owned");
-    let x_len = reference(&x);
-    println!("{x} {x_len}");
-
+    let information = strcture_test("example@gmail.com".to_string(),"TestName".to_string(),21);
+    println!("{} {} {}",information.mail,information.name,information.age);
+    let information2 = Info{
+        age: 22,
+        name: String::from("TestChangedName"),
+        ..information
+    };
+    println!("{} {} {}",information2.mail,information2.name,information2.age);
 }
+
 
 // fn imp_return(x: f32, y:f32)-> f32 {
 //     x / y
@@ -28,6 +38,20 @@ fn main() {
 //     println!("{some_string}");
 // }
 
-fn reference(x:&String)->usize{
-    x.len()
+// fn reference(x:&String)->usize{
+//     x.len()
+// }
+// fn slicer_test(x:String){
+
+//     let sliced = &x[..3].len();
+//     println!("{x} {sliced}");
+// }
+
+fn strcture_test(mail:String,name:String,age:u8)-> Info {
+    Info{
+        mail,
+        name,
+        age,
+    }
+    
 }
