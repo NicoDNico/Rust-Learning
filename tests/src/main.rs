ed6dda8b409bfcabfead1919fb9e8b7aa86c23f1
NicoDNico <Nicolasdnicora@gmail.com>
2023-04-01 20:13:18 -0300

    Structs but classes but object but only info ?
    Maybe kotlin was the better choice. But im afraid to learn that maybe kotling also has inheritance.
    shouldnt structs only be used for data ?  maybe the book lied to me.

--- tests/src/main.rs
+++ tests/src/main.rs
@@ -1,59 +1,78 @@
 
 
-struct Info {
-    mail: String,
-    name: String,
-    age: u8,
-}
 
 fn main() {
-    let information = strcture_test("example@gmail.com".to_string(),"TestName".to_string(),21);
-    println!("{} {} {}",information.mail,information.name,information.age);
-    let information2 = Info{
-        age: 22,
-        name: String::from("TestChangedName"),
-        ..information
-    };
-    println!("{} {} {}",information2.mail,information2.name,information2.age);
+    
+    let stats = info_builder(String::from("UserTest"),122,102,211);
+    println!("{:#?}",stats);
+    println!("Ratio:{}",stats.ratio())
 }
+
 
 
 // fn imp_return(x: f32, y:f32)-> f32 {
-//     x / y
-// }
-
-
-// fn scoped(){
-//     let s: &str= "no";
-
-//     {
-//         let mut s: String = String::from("yes");
-//         s.push_str(" maybe");
-//         println!("{s}");
-//     }
-
-//     println!("{s}");
-
-// }
-
-// fn ownership_test(some_string:&str){
-//     println!("{some_string}");
-// }
-
-// fn reference(x:&String)->usize{
-//     x.len()
-// }
-// fn slicer_test(x:String){
-
-//     let sliced = &x[..3].len();
-//     println!("{x} {sliced}");
-// }
-
-fn strcture_test(mail:String,name:String,age:u8)-> Info {
-    Info{
-        mail,
-        name,
-        age,
-    }
+    //     x / y
+    // }
     
-}
+    
+    // fn scoped(){
+        //     let s: &str= "no";
+        
+        //     {
+            //         let mut s: String = String::from("yes");
+    //         s.push_str(" maybe");
+    //         println!("{s}");
+    //     }
+    
+    //     println!("{s}");
+    
+    // }
+    
+    // fn ownership_test(some_string:&str){
+        //     println!("{some_string}");
+        // }
+        
+        // fn reference(x:&String)->usize{
+            //     x.len()
+            // }
+            // fn slicer_test(x:String){
+                
+                //     let sliced = &x[..3].len();
+                //     println!("{x} {sliced}");
+                // }
+                
+                
+                // struct Info {
+                    //     mail: String,
+                    //     name: String,
+                    //     age: u8,
+                    // }
+                    // fn strcture_test(mail:String,name:String,age:u8)-> Info {
+                        //     Info{
+        //         mail,
+        //         name,
+        //         age,
+        //     }
+        
+        // }
+    #[derive(Debug)]
+    struct Info {
+        user:String,
+        kills: u32,
+        deaths:u32,
+        assists:u32,
+    }
+    impl Info{
+        fn ratio(&self)-> f32{
+            (self.kills + self.assists ) as f32 / self.deaths as f32 
+        }
+    }
+        
+    fn info_builder(user:String , kills: u32 , deaths:u32, assists:u32) ->Info{
+        Info{
+            user,
+            kills,
+            deaths,
+            assists,
+        }
+    }
