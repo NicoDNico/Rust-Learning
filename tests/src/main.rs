5a0f3961c4025a1303e7b5d14b9eede9516dc3e4
NicoDNico <Nicolasdnicora@gmail.com>
2023-04-01 02:19:31 -0300

    Testing borrowing.
    So i finally understood whats the difference between &str and String. i guess "&" is the symbol for pointer or some shit.

--- tests/src/main.rs
+++ tests/src/main.rs
@@ -1,10 +1,29 @@
 
 
 fn main() {
-    println!("{}",test_func(32.0,1.1));
+    let x:&str = "Owned";
+    ownership_test(x);
+    println!("{x}");
 
 }
 
-fn test_func(x: f32, y:f32)-> f32 {
-    x / y
+// fn imp_return(x: f32, y:f32)-> f32 {
+//     x / y
+// }
+
+// fn scoped(){
+//     let s: &str= "no";
+
+//     {
+//         let mut s: String = String::from("yes");
+//         s.push_str(" maybe");
+//         println!("{s}");
+//     }
+
+//     println!("{s}");
+
+// }
+
+fn ownership_test(some_string:&str){
+    println!("{some_string}");
 }
